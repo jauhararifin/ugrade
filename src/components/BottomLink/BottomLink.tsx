7c5a8302a36f29499e4f4a9e3b912b292cf4b5de
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-03 19:13:35 +0700

    add simple bottom link component

---
+++ src/components/BottomLink/BottomLink.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react'
+
+import './BottomLink.css'
+
+export interface BottomLinkProps {
+    children: ReactNode
+    dividerColor?: string
+}
+
+const BottomLink: React.SFC<BottomLinkProps> = ({ children, dividerColor }) => (
+    <div className="bottom-link">
+        {
+            React.Children.map(children, item => <span style={{ borderRightColor: dividerColor }}>{item}</span>)
+        }
+    </div>
+)
+
+BottomLink.defaultProps = {
+    children: <React.Fragment />,
+    dividerColor: 'black'
+}
+
+export default BottomLink
