4b0195b7c10469c6940f0fde89c84e2a166ed46a
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-04 10:24:33 +0700

    migrate home page using blueprintjs

--- src/scenes/HomePage/HomePage.tsx
+++ src/scenes/HomePage/HomePage.tsx
@@ -1,51 +1,43 @@
 import React from 'react'
-import { Button, Typography, Grid, Divider } from '@material-ui/core'
+import { Button, Intent } from '@blueprintjs/core'
 import { connect } from 'react-redux'
 import { Path, LocationState } from 'history'
-import { push, CallHistoryMethodAction } from 'connected-react-router'
 import { Link } from 'react-router-dom'
+import { push, CallHistoryMethodAction } from 'connected-react-router'
 
-import pattern from '../../assets/images/pattern-light.svg'
 import logo from '../../assets/images/logo.svg'
-import BottomLink from '../../components/BottomLink/BottomLink'
 
-import './HomePage.css'
+import './styles.css'
+import BottomLink from '../../components/BottomLink'
 
 export interface HomePageProps {
   push(path: Path, state?: LocationState): CallHistoryMethodAction
 }
 
 const HomePage: React.SFC<HomePageProps> = ({ push }) => (
-  <React.Fragment>
-    <Grid container className='home-page' style={{ backgroundImage: `url(${pattern})`}} >
-      <Grid item md={4} />
-      <Grid item direction='column' className='home-page-content' md={4}>
-        <div className='home-page-logo'>
-          <img src={logo} alt='logo' width={100} />
-        </div>
-        <div className='home-page-title'>
-          <Typography variant='h3'>
-            Welcome To UGrade
-          </Typography>
-        </div>
-        <div className='home-page-signin'>
-          <Button size="large" variant="contained" fullWidth onClick={() => push('/signin')}>
-            Sign In
-          </Button>
-        </div>
-        <Divider />
-        <div className='home-page-signup'>
-          <Button size="large" variant="contained" color="primary" fullWidth onClick={() => push('/signup')}>
-            Sign Up
-          </Button>
-        </div>
-      </Grid>
-      <Grid item md={4} />
-    </Grid>
+  <div className="home-page">
+    <div className="home-panel">
+      <Link to="/">
+        <img src={logo} width={100} alt="logo" />
+        </Link>
+      <h1>Welcom To UGrade</h1>
+      <h4>
+        There is no one who loves pain itself, who seeks after it and wants to
+        have it, simply because it is pain...
+      </h4>
+      <div className="home-actions">
+        <Button fill large className="item" onClick={() => push('/signup')}>
+          Sign Up
+        </Button>
+        <Button fill large className="item" intent={Intent.SUCCESS} onClick={() => push('/signin')}>
+          Sign In
+        </Button>
+      </div>
+    </div>
     <BottomLink>
-      <Link to='/setting'><Typography variant='body1' color='secondary'>Setting</Typography></Link>
+      <Link to="/setting">Settings</Link>
     </BottomLink>
-  </React.Fragment>
+  </div>
 )
 
 export default connect(null, { push })(HomePage)
