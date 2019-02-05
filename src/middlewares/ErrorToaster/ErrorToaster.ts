e3b905baabf36854059fc7d817378ffd0c88983b
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-05 08:18:23 +0700

    fix error toaster typing

--- src/middlewares/ErrorToaster/ErrorToaster.ts
+++ src/middlewares/ErrorToaster/ErrorToaster.ts
@@ -1,14 +1,14 @@
 import Toaster, { ActionToaster } from "./ActionToaster"
-import { Middleware } from "redux"
+import { Middleware, Action } from "redux"
 
 export function createErrorToasterMiddleware(toaster: ActionToaster): Middleware {
-    return store => next => async action => {
-        try {
-            return await next(action)
-        } catch (error) {
+    return store => next => <A extends Action>(action: A):A => {
+        const result = next(action)
+        const promise = Promise.resolve(result).catch(error => {
             toaster.showErrorToast(error)
             throw error
-        }
+        })
+        return <A><unknown>promise
     }      
 }
 
