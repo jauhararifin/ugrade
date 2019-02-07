8d1a3dbb2616a3655049b112f2105fcc2b4fe6a1
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-07 09:04:09 +0700

    add auth helper to automatically redirect signed in user

---
+++ src/helpers/auth/publicOnly.tsx
@@ -0,0 +1,22 @@
+import React, { ComponentType, SFC } from 'react'
+import { Redirect } from 'react-router'
+import { connect } from 'react-redux'
+
+import { AppState } from '../../stores'
+
+export const publicOnly = (redirect: string = "/") =>
+    <P extends object>(Component: ComponentType<P>) => {
+        interface props {
+            signedIn: boolean
+        }
+        const result: SFC<P & props> = (props) => {
+            const { signedIn } = props
+            if (signedIn)
+                return <Redirect to={redirect} />
+            return <Component {...props} />
+        }
+        const mapStateToProps = (state: AppState): props => ({
+            signedIn: state.auth.isSignedIn,
+        })
+        return connect(mapStateToProps)(result as any)
+    }
