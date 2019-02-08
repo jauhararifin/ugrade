5b22b34eb8aea33757adb06187b8190b6c2e21f7
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-08 09:02:23 +0700

    add transition when changin page

--- src/scenes/App.tsx
+++ src/scenes/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Route, withRouter } from 'react-router-dom'
+import { Route, withRouter, Switch } from 'react-router-dom'
 import DocumentTitle from 'react-document-title'
 import { connect } from 'react-redux'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { Location } from 'history'
+
+import "./styles.css"
 
 import { AppState } from '../stores'
-
 import Home from './Home'
 import SignIn from './SignIn'
 import Setting from './Setting'
@@ -16,23 +19,28 @@ import MyAccount from './MyAccount'
 
 export interface AppProps {
   title?: string
+  location: Location
 }
 
-const App: React.SFC<AppProps> = ({ title }) => (
+const App: React.SFC<AppProps> = ({ title, location }) => (
   <DocumentTitle title={title || "UGrade"}>
-    <React.Fragment>
-      <Route path='/' exact component={Home} />
-      <Route path='/signin' component={SignIn} />
-      <Route path='/signup' component={SignUp} />
-      <Route path='/setting' component={Setting} />
-      <Route path='/account' exact component={MyAccount} />
-      <Route path='/forgot-password' component={ForgotPassword} />
-      <Route path='/contests' exact component={Contests} />
-      <Route path='/contests/:contestId' component={ContestDetail} />
-    </React.Fragment>
+    <TransitionGroup>
+      <CSSTransition timeout={300} classNames="fade" key={location.key}>
+        <Switch location={location}>
+          <Route path='/' exact component={Home} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/setting' component={Setting} />
+          <Route path='/account' exact component={MyAccount} />
+          <Route path='/forgot-password' component={ForgotPassword} />
+          <Route path='/contests' exact component={Contests} />
+          <Route path='/contests/:contestId' component={ContestDetail} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   </DocumentTitle>
 )
 
-const mapStateToProps = (state: AppState) => ({ title: state.title, })
+const mapStateToProps = (state: AppState) => ({ title: state.title })
 
 export default withRouter(connect(mapStateToProps)(App) as any)
