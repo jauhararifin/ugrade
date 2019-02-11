9e98fe5817810a14ef13657310a23fa25a782577
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-11 17:47:06 +0700

    add subscribe contest announcements in contest service

--- src/services/contest/ContestService.ts
+++ src/services/contest/ContestService.ts
@@ -1,10 +1,21 @@
 import { Announcement } from './Announcement'
 import { Contest } from './Contest'
 
+export type AnnouncementSubscribeCallback = (
+  announcements: Announcement[]
+) => any
+
+export type AnnouncementUbsubscribeFunction = () => any
+
 export interface ContestService {
   getAllContests(): Promise<Contest[]>
   getContestById(id: number): Promise<Contest>
   getAccouncementsByContestId(contestId: number): Promise<Announcement[]>
+
+  subscribeAnnouncements(
+    contestId: number,
+    callback: AnnouncementSubscribeCallback
+  ): AnnouncementUbsubscribeFunction
 
   readAnnouncements(token: string, id: number[]): Promise<void>
 }
--- src/services/contest/InMemoryContestService.ts
+++ src/services/contest/InMemoryContestService.ts
@@ -1,7 +1,11 @@
 import { AuthService } from '../auth'
 import { Announcement } from './Announcement'
 import { Contest } from './Contest'
-import { ContestService } from './ContestService'
+import {
+  AnnouncementSubscribeCallback,
+  AnnouncementUbsubscribeFunction,
+  ContestService,
+} from './ContestService'
 import { NoSuchContest } from './errors'
 import { annoucements, contests } from './fixtures'
 
@@ -11,7 +15,11 @@ export class InMemoryContestService implements ContestService {
 
   constructor(authService: AuthService) {
     this.authService = authService
+
     this.contests = contests
+    this.contests.forEach(
+      contest => (contest.announcements = annoucements.slice())
+    )
   }
 
   async getAllContests(): Promise<Contest[]> {
@@ -36,11 +44,45 @@ export class InMemoryContestService implements ContestService {
   ): Promise<Announcement[]> {
     await new Promise(resolve => setTimeout(resolve, 1500))
     if (contestId === 0) throw new Error('Connection Error')
-    return annoucements
+    if (!this.contests[contestId]) throw new NoSuchContest('No Such Contest')
+    const contest = this.contests[contestId]
+    if (contest.announcements) {
+      return contest.announcements.slice()
+    }
+    return []
   }
 
   async readAnnouncements(token: string, id: number[]): Promise<void> {
     await this.authService.getMe(token)
-    annoucements.filter(i => i.id in id).forEach(val => (val.read = true))
+    annoucements
+      .filter(i => id.includes(i.id))
+      .forEach(val => (val.read = true))
+  }
+
+  subscribeAnnouncements(
+    contestId: number,
+    callback: AnnouncementSubscribeCallback
+  ): AnnouncementUbsubscribeFunction {
+    const runThis = async () => {
+      const newAnnouncement: Announcement = {
+        id: Math.round(Math.random() * 300),
+        title: `Lorem Ipsum ${Math.random()}`,
+        content: `Lorem ipsum dos color sit amet`,
+        issuedTime: new Date(Date.now()),
+        read: false,
+      }
+
+      const arr = (this.contests[contestId].announcements || []).slice()
+      arr.push(newAnnouncement)
+      this.contests[contestId].announcements = arr
+
+      const announcements = await this.getAccouncementsByContestId(contestId)
+      callback(announcements)
+    }
+
+    const timeout = setInterval(runThis, (10 + Math.random() * 5) * 1000)
+    return () => {
+      clearInterval(timeout)
+    }
   }
 }
