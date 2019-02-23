8deafdb3e1df270f61fc89c6e00bd1e63f810e35
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-23 09:29:33 +0700

    fix user state in auth store

--- frontend/src/stores/Auth/AuthState.ts
+++ frontend/src/stores/Auth/AuthState.ts
@@ -2,28 +2,10 @@ export const AUTH_IS_SIGNED_IN_KEY = 'auth.isSignedIn'
 export const AUTH_TOKEN_KEY = 'auth.token'
 export const AUTH_REMEMBER_ME_KEY = 'auth.rememberMe'
 
-export enum GenderType {
-  GENDER_TYPE_MALE = 'Male',
-  GENDER_TYPE_FEMALE = 'Female',
-}
-
-export enum ShirtSizeType {
-  SHIRT_SIZE_TYPE_XS = 'XS',
-  SHIRT_SIZE_TYPE_S = 'S',
-  SHIRT_SIZE_TYPE_M = 'M',
-  SHIRT_SIZE_TYPE_L = 'L',
-  SHIRT_SIZE_TYPE_XL = 'XL',
-  SHIRT_SIZE_TYPE_XXL = 'XXL',
-}
-
 export interface User {
   username: string
   name: string
   email: string
-
-  gender?: GenderType
-  address?: string
-  shirtSize?: ShirtSizeType
 }
 
 export interface AuthState {
