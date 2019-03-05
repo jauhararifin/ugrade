0eb22014a778aefc407c9c574c3d2485d3d93a84
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-05 22:02:29 +0700

    add doc in contest service

--- frontend/src/services/contest/ContestService.ts
+++ frontend/src/services/contest/ContestService.ts
@@ -9,12 +9,50 @@ export type SubscriptionCallback<T> = (newItem: T) => any
 export type UnsubscriptionFunction = () => any
 
 export interface ContestService {
+  /**
+   * Get all available languages that supported by ugrade system.
+   * @returns List of Language that supported by ugrade.
+   */
   getAvailableLanguages(): Promise<Language[]>
 
+  /**
+   * Get contest information by it's short id. Short id is used by user to
+   * identify a contest. Short id is a unique identifier for contest that human
+   * friendly.
+   * @param shortId Short id of contest.
+   * @returns Contest information
+   */
   getContestByShortId(shortId: string): Promise<Contest>
 
+  /**
+   * Get user's contest by their token. Every authenticated user have a token
+   * that identifies their session. This returns user's current contest info.
+   * @param token User's session token
+   * @returns User's curent contest
+   */
   getMyContest(token: string): Promise<Contest>
 
+  /**
+   * Update contest information. To do this, user's need `info:update`
+   * permission.
+   * @param token User's session token to identify contest
+   * @param name Contest name, Ignored when the value is undefined
+   * @param shortDescription Contest short description, Short description will
+   * shows in the login page after choose contest. Ignored when the value is
+   * undefined
+   * @param description Contest description. This contains markdown string that
+   * describe the contest, may contains contest rule, price, etc.
+   * @param startTime Contest start time, contest is considered started when
+   * the current time is between `startTime` and `finishTime`
+   * @param freezed Is contest freezed. When the contest freezed, scoreboard
+   * will not updated when a user submit solutions.
+   * @param finishTime Contest finish time, contest is considered started when
+   * the current time is between `startTime` and `finishTime`. After finish,
+   * contestant cannot submit solution.
+   * @param permittedLanguages Contest permitted languages. All languages that
+   * permitted to be used in the contest.
+   * @returns The new contest information after updated
+   */
   updateContestInfo(
     token: string,
     name?: string,
