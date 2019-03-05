bf680c87b05ed7a1f28267410f5ef668c09aa645
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-05 10:37:09 +0700

    remove can edit in overview page

--- frontend/src/scenes/Dashboard/Overview/Overview.tsx
+++ frontend/src/scenes/Dashboard/Overview/Overview.tsx
@@ -1,13 +1,11 @@
 import React, { FunctionComponent } from 'react'
-import { useContestOnly, usePermissions } from 'ugrade/auth'
-import { UserPermission } from 'ugrade/auth/store'
+import { useContestOnly } from 'ugrade/auth'
 import { useContestInfo } from 'ugrade/contest'
 import { OverviewView } from './OverviewView'
 
 export const Overview: FunctionComponent = () => {
   useContestOnly()
   const contest = useContestInfo()
-  const canEdit = usePermissions([UserPermission.InfoUpdate])
 
-  return <OverviewView canEdit={canEdit} contest={contest} />
+  return <OverviewView contest={contest} />
 }
