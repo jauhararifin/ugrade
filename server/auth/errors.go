2a6808fd9430abc8f9157eba191defd6e5379c82
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-13 21:25:30 +0700

    fix auth service contracts

--- server/auth/errors.go
+++ server/auth/errors.go
@@ -1,63 +1,122 @@
 package auth
 
-type errorString struct {
-	msg string
+// NoSuchUser indicates indicates that no user is found when calling Service methods.
+type NoSuchUser interface {
+	NoSuchUser() bool
 }
 
-func (e *errorString) Error() string {
-	return e.msg
+// IsNoSuchUser checks whether error is NoSuchUser
+func IsNoSuchUser(err error) bool {
+	if m, ok := err.(NoSuchUser); ok {
+		return m.NoSuchUser()
+	}
+	return false
 }
 
-// NoSuchUserError indicates indicates that no user is found when calling Service methods.
-type NoSuchUserError struct {
-	errorString
+// NoSuchContest indicates indicates that no contest is found when calling service methods.
+type NoSuchContest interface {
+	NoSuchContest() bool
 }
 
-// NoSuchContestError indicates indicates that no contest is found when calling service methods.
-type NoSuchContestError struct {
-	errorString
+// IsNoSuchContest checks whether error is NoSuchContest
+func IsNoSuchContest(err error) bool {
+	if m, ok := err.(NoSuchContest); ok {
+		return m.NoSuchContest()
+	}
+	return false
 }
 
-// WrongCredentialError indicates indicates that provided credential is wrong when authenticating user.
-type WrongCredentialError struct {
-	errorString
+// WrongCredential indicates indicates that provided credential is wrong when authenticating user.
+type WrongCredential interface {
+	WrongCredential() bool
 }
 
-// WrongTokenError indicates indicates that user provided wrong token. oneTimeCode is considered as token too.
-type WrongTokenError struct {
-	errorString
+// IsWrongCredential checks whether error is WrongCredential
+func IsWrongCredential(err error) bool {
+	if m, ok := err.(WrongCredential); ok {
+		return m.WrongCredential()
+	}
+	return false
 }
 
-// InvalidSessionError indicates indicates that user provided invalid session token.
-type InvalidSessionError struct {
-	errorString
+// WrongToken indicates indicates that user provided wrong token. oneTimeCode is considered as token too.
+type WrongToken interface {
+	WrongToken() bool
 }
 
-// ForbiddenActionError indicates indicates that user doesn't have permission to do some actions.
-type ForbiddenActionError struct {
-	errorString
+// IsWrongToken checks whether error is WrongToken
+func IsWrongToken(err error) bool {
+	if m, ok := err.(WrongToken); ok {
+		return m.WrongToken()
+	}
+	return false
 }
 
-// InvalidInputError indiates that user's input is invalid.
-type InvalidInputError struct {
-	errorString
-	Username string
-	Name     string
-	Email    string
-	Password string
+// InvalidSession indicates indicates that user provided invalid session token.
+type InvalidSession interface {
+	InvalidSession() bool
 }
 
-// UsernameAlreadyUsedError indicates indicates that user's username input is already used by other user.
-type UsernameAlreadyUsedError struct {
-	errorString
+// IsInvalidSession checks whether error is InvalidSession
+func IsInvalidSession(err error) bool {
+	if m, ok := err.(InvalidSession); ok {
+		return m.InvalidSession()
+	}
+	return false
 }
 
-// InternalServerError indicates indicates that there is internal server error.
-type InternalServerError struct {
-	errorString
+// ForbiddenAction indicates indicates that user doesn't have permission to do some actions.
+type ForbiddenAction interface {
+	ForbiddenAction() bool
 }
 
-// AlreadyInvitedError indicates indicates that users is already invited.
-type AlreadyInvitedError struct {
-	errorString
+// IsForbiddenAction checks whether error is ForbiddenAction
+func IsForbiddenAction(err error) bool {
+	if m, ok := err.(ForbiddenAction); ok {
+		return m.ForbiddenAction()
+	}
+	return false
+}
+
+// InvalidInput indiates that user's input is invalid.
+type InvalidInput interface {
+	InvalidInput() bool
+	Username() string
+	Name() string
+	Email() string
+	Password() string
+}
+
+// IsInvalidInput checks whether error is InvalidInput
+func IsInvalidInput(err error) bool {
+	if m, ok := err.(InvalidInput); ok {
+		return m.InvalidInput()
+	}
+	return false
+}
+
+// UsernameAlreadyUsed indicates indicates that user's username input is already used by other user.
+type UsernameAlreadyUsed interface {
+	UsernameAlreadyUsed() bool
+}
+
+// IsUsernameAlreadyUsed checks whether error is UsernameAlreadyUsed
+func IsUsernameAlreadyUsed(err error) bool {
+	if m, ok := err.(UsernameAlreadyUsed); ok {
+		return m.UsernameAlreadyUsed()
+	}
+	return false
+}
+
+// AlreadyInvited indicates indicates that users is already invited.
+type AlreadyInvited interface {
+	AlreadyInvited() bool
+}
+
+// IsAlreadyInvited checks whether error is AlreadyInvited
+func IsAlreadyInvited(err error) bool {
+	if m, ok := err.(AlreadyInvited); ok {
+		return m.AlreadyInvited()
+	}
+	return false
 }
--- server/auth/service.go
+++ server/auth/service.go
@@ -3,41 +3,41 @@ package auth
 // Service is the interface that wraps authentication functionality in UGrade.
 type Service interface {
 	// UserByID returns specific User by its ID. If no such user is found, then returned ErrNoSuchUser.
-	UserByID(userID string) (*User, Error)
+	UserByID(userID string) (*User, error)
 
 	// UsersInContest returns all users in a contest. If no such contest is found, then returned ErrNoSuchContest.
-	UsersInContest(contestID string) ([]*User, Error)
+	UsersInContest(contestID string) ([]*User, error)
 
 	// UserByEmail returns specific user by combination if its contestID and email. When no such user if found, returned ErrNoSuchUser.
-	UserByEmail(contestID, email string) (*User, Error)
+	UserByEmail(contestID, email string) (*User, error)
 
 	// UserByUsernames returns slices of users by its username in specific contest. When no such contest if found, returned ErrNoSuchContest.
-	UserByUsernames(contestID string, usernames []string) ([]*User, Error)
+	UserByUsernames(contestID string, usernames []string) ([]*User, error)
 
-	// SignIn authenticate combination of contestId, email and password and returns session token when success, otherwise return ErrWrongCredential Error.
-	SignIn(contestID, email, password string) (string, Error)
+	// SignIn authenticate combination of contestId, email and password and returns session token when success, otherwise return ErrWrongCredential error.
+	SignIn(contestID, email, password string) (string, error)
 
 	// SignUp used by used when they first time join the contest. It takes user information such as name, username and passwod and returned session token when success.
-	SignUp(contestID, username, email, oneTimeCode, password, name string) (string, Error)
+	SignUp(contestID, username, email, oneTimeCode, password, name string) (string, error)
 
 	// ForgotPassword used by user to send reset password request.
-	ForgotPassword(contestID, email string) Error
+	ForgotPassword(contestID, email string) error
 
 	// ResetPasswowrd used by user to reset their password.
-	ResetPassword(contestID, email, oneTimeCode, password string) Error
+	ResetPassword(contestID, email, oneTimeCode, password string) error
 
 	// AddUser invite other users to a contest.
-	AddUser(token string, users map[string][]int) Error
+	AddUser(token string, users map[string][]int) error
 
 	// Me returns User information based on their's session token.
-	Me(token string) (*User, Error)
+	Me(token string) (*User, error)
 
 	// SetMyPassword updates user password.
-	SetMyPassword(token, oldPassword, newPassword string) Error
+	SetMyPassword(token, oldPassword, newPassword string) error
 
 	// SetMyName updates user's name information.
-	SetMyName(token, name string) Error
+	SetMyName(token, name string) error
 
 	// SetUserPermission updates other user's permission list.
-	SetUserPermissions(token, userID string, permissions []int) Error
+	SetUserPermissions(token, userID string, permissions []int) error
 }
