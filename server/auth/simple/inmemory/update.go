8321ffd268c7c20f9405fa76a16a2d833ca418b5
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-14 16:42:14 +0700

    break update method into new file

---
+++ server/auth/simple/inmemory/update.go
@@ -0,0 +1,49 @@
+package inmemory
+
+import (
+	"fmt"
+
+	"github.com/jauhararifin/ugrade/server/auth/simple"
+	"github.com/pkg/errors"
+)
+
+func (m *inMemory) Update(userID string, newUser *simple.User) error {
+	user, err := m.UserByID(userID)
+	if err != nil {
+		if _, ok := err.(*noSuchUser); ok {
+			return &noSuchUser{}
+		}
+		return errors.Wrap(err, "cannot store updated user")
+	}
+
+	delete(m.mapIDUser, user.ID)
+	m.mapIDUser[userID] = newUser
+
+	if user.ContestID != newUser.ContestID {
+		oldContestMap := m.mapContestUsers[user.ContestID]
+		for i, uid := range oldContestMap {
+			if uid == user.ContestID {
+				m.mapContestUsers[user.ContestID] = append(oldContestMap[:i], oldContestMap[i+1:]...)
+				break
+			}
+		}
+
+		newContestMap := m.mapContestUsers[newUser.ContestID]
+		newContestMap = append(newContestMap, newUser.ID)
+	}
+
+	oldEmailKey := fmt.Sprintf("%s/%s", user.ContestID, user.Email)
+	newEmailKey := fmt.Sprintf("%s/%s", user.ContestID, newUser.Email)
+	delete(m.mapContestEmail, oldEmailKey)
+	m.mapContestEmail[newEmailKey] = newUser.ID
+
+	oldUsernameKey := fmt.Sprintf("%s/%s", user.ContestID, user.Username)
+	newUsernameKey := fmt.Sprintf("%s/%s", user.ContestID, newUser.Username)
+	delete(m.mapContestUsername, oldUsernameKey)
+	m.mapContestUsername[newUsernameKey] = newUser.ID
+
+	delete(m.mapToken, user.Token)
+	m.mapToken[newUser.Token] = newUser.ID
+
+	return nil
+}
