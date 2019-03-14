64c0524db0b2db230042cc7abf1b5693f7c5de6a
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-14 14:17:42 +0700

    add test for get users in contest

--- server/auth/simple/inmemory/users_in_contest_test.go
+++ server/auth/simple/inmemory/users_in_contest_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/jauhararifin/ugrade/server/auth"
-
 	"github.com/jauhararifin/ugrade/server/auth/simple"
 )
 
@@ -37,5 +36,51 @@ func TestUsersInContest(t *testing.T) {
 	}
 	if user2 != users[0] && user2 != users[1] {
 		t.Error("Expecting users contain user2")
+	}
+}
+
+func TestUsersInContestNoSuchContest(t *testing.T) {
+	user1 := &simple.User{
+		User: &auth.User{
+			ID:        "userone",
+			ContestID: "contestid",
+		},
+	}
+	user2 := &simple.User{
+		User: &auth.User{
+			ID:        "usertwo",
+			ContestID: "contestid",
+		},
+	}
+	m := &inMemory{
+		mapIDUser:       map[string]*simple.User{user1.ID: user1, user2.ID: user2},
+		mapContestUsers: map[string][]string{"contestid": []string{"userone", "usertwo"}},
+	}
+	users, err := m.UsersInContest("othercontest")
+	if users != nil {
+		t.Errorf("Expecting users to be null, found %v instead", users)
+	}
+	if err == nil {
+		t.Errorf("Expecting error to be noSuchContest, found nil intead")
+	}
+	if _, ok := err.(*noSuchContest); !ok {
+		t.Errorf("Expecting error to be noSuchContest, found %T intead", err)
+	}
+}
+
+func TestUsersInContestInvalidState(t *testing.T) {
+	m := &inMemory{
+		mapIDUser:       make(map[string]*simple.User),
+		mapContestUsers: map[string][]string{"contestid": []string{"userone", "usertwo"}},
+	}
+	users, err := m.UsersInContest("contestid")
+	if users != nil {
+		t.Errorf("Expecting users to be null, found %v instead", users)
+	}
+	if err == nil {
+		t.Errorf("Expecting error to be invalidState, found nil intead")
+	}
+	if _, ok := err.(*invalidState); !ok {
+		t.Errorf("Expecting error to be invalidState, found %T intead", err)
 	}
 }
