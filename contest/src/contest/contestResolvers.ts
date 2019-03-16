b3b64e3c50c5a09ecc9baefc2cec975cb46ee133
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-16 12:33:44 +0700

    remove user and contest resolver, implement in auth instead

--- contest/src/contest/contestResolvers.ts
+++ contest/src/contest/contestResolvers.ts
@@ -5,7 +5,6 @@ import { UserModel } from '../user/store'
 
 export interface ContestResolvers {
   Query: {
-    contest: IFieldResolver<any, AppContext, any>
     contestById: IFieldResolver<any, any, { id: string }>
     contestByShortId: IFieldResolver<any, any, { shortId: string }>
   }
@@ -18,10 +17,6 @@ export const createContestResolvers = (
   store: ContestStore
 ): ContestResolvers => ({
   Query: {
-    contest: (_parent, _args, { authToken }) => {
-      return null
-    },
-
     contestById: (_parent, { id }) => store.getContestById(id),
 
     contestByShortId: (_parent, { shortId }) =>
--- contest/src/user/userResolvers.ts
+++ contest/src/user/userResolvers.ts
@@ -5,7 +5,6 @@ import { ContestModel } from '../contest/store'
 
 export interface UserResolvers {
   Query: {
-    user: IFieldResolver<any, AppContext, any>
     userById: IFieldResolver<any, any, { id: string }>
     userByEmail: IFieldResolver<any, any, { contestId: string; email: string }>
     userByUsername: IFieldResolver<
@@ -21,7 +20,6 @@ export interface UserResolvers {
 
 export const createUserResolvers = (store: UserStore): UserResolvers => ({
   Query: {
-    user: () => null,
     userById: (_source, { id }) => store.getUserById(id),
     userByEmail: (_source, { contestId, email }) =>
       store.getUserByEmail(contestId, email),
