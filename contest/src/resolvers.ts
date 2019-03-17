d8e8c516682bb89bd717489935fc7ff38f785bf7
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-17 20:37:05 +0700

    add typechecking for resolvers

--- contest/src/resolvers.ts
+++ contest/src/resolvers.ts
@@ -1,14 +1,38 @@
 import { GraphQLResolveInfo } from 'graphql'
 import { IResolvers, MergeInfo } from 'graphql-tools'
 import { merge } from 'lodash'
-import { createAuthResolvers } from './auth'
+import {
+  createAuthResolvers,
+  UserByEmailResolver,
+  UserByIdResolver,
+  UserByTokenResolver,
+  UserByUsernameResolver,
+} from './auth'
+import { AddUserResolver } from './auth/addUserResolver'
+import { ForgotPasswordResolver } from './auth/forgotPasswordResolver'
+import { ResetPasswordResolver } from './auth/resetPasswordResolver'
+import { SetMyNameResolver } from './auth/setMyNameResolver'
+import { SetMyPasswordResolver } from './auth/setMyPasswordResolver'
+import { SetPermissionsResolver } from './auth/setPermissionsResolver'
+import { SigninResolver } from './auth/signinResolver'
+import { SignupResolver } from './auth/signupResolver'
 import { AuthStore } from './auth/store'
-import { createContestResolvers } from './contest'
+import {
+  ContestByIdResolver,
+  ContestByShortIdResolver,
+  createContestResolvers,
+} from './contest'
 import { ContestStore } from './contest/store'
 import { AppContext } from './context'
+import { AllLanguageResolver, LanguageByIdResolver } from './language'
 import { createLanguageResolvers } from './language/languageResolvers'
 import { LanguageStore } from './language/store'
-import { createProfileResolvers } from './profile'
+import {
+  createProfileResolvers,
+  ProfileByTokenResolver,
+  SetMyProfileResolver,
+  UserProfileResolver,
+} from './profile'
 import { ProfileStore } from './profile/store'
 
 export type AppFieldResolver<TSource = any, TArgs = any, TReturn = any> = (
@@ -20,12 +44,49 @@ export type AppFieldResolver<TSource = any, TArgs = any, TReturn = any> = (
   }
 ) => TReturn
 
+export interface AppQueryResolver {
+  user: UserByTokenResolver
+  userById: UserByIdResolver
+  userByEmail: UserByEmailResolver
+  userByUsername: UserByUsernameResolver
+
+  contestById: ContestByIdResolver
+  contestByShortId: ContestByShortIdResolver
+
+  languages: AllLanguageResolver
+  languageById: LanguageByIdResolver
+
+  profile: ProfileByTokenResolver
+  userProfile: UserProfileResolver
+}
+
+export interface AppMutationResolver {
+  signin: SigninResolver
+  signup: SignupResolver
+  forgotPassword: ForgotPasswordResolver
+  resetPassword: ResetPasswordResolver
+  addUser: AddUserResolver
+  setMyPassword: SetMyPasswordResolver
+  setMyName: SetMyNameResolver
+  setPermissions: SetPermissionsResolver
+
+  setMyProfile: SetMyProfileResolver
+
+  // updateContest
+  // createContest
+}
+
+export interface AppResolver {
+  Query: AppQueryResolver
+  Mutation: AppMutationResolver
+}
+
 export function createResolvers(
   contestStore: ContestStore,
   languageStore: LanguageStore,
   authStore: AuthStore,
   profileStore: ProfileStore
-): IResolvers {
+): AppResolver {
   const contestResolvers = createContestResolvers(contestStore)
   const languageResolvers = createLanguageResolvers(languageStore)
   const authResolvers = createAuthResolvers(authStore)
@@ -35,5 +96,5 @@ export function createResolvers(
     languageResolvers,
     authResolvers,
     profileResolvers
-  ) as any
+  )
 }
