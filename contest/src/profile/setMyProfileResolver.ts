0d90b28afd4b36bbbbcdde0629a526ad76dd98d9
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-17 07:00:38 +0700

    set user profile

--- contest/src/profile/setMyProfileResolver.ts
+++ contest/src/profile/setMyProfileResolver.ts
@@ -2,6 +2,7 @@ import { IFieldResolver } from 'graphql-tools'
 import { userByTokenResolver } from 'ugrade/auth'
 import { AuthStore } from 'ugrade/auth/store'
 import { AppContext } from 'ugrade/context'
+import { logger } from 'ugrade/logger'
 import { GenderType, ProfileStore, ShirtSizeType } from './store'
 
 export type SetMyProfileResolver = IFieldResolver<
@@ -17,6 +18,8 @@ export function setMyProfileResolver(
   return async (source, args, context, info) => {
     const userByToken = userByTokenResolver(authStore)
     const me = await userByToken(source, args, context, info)
+
+    logger.info('User %s/%s set profile', me.contestId, me.id, args)
     return profileStore.putProfile({
       userId: me.id,
       ...args,
