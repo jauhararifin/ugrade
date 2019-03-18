c032d6c96a3c4f678aebe9a5d9e134ec938ae198
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-18 21:23:50 +0700

    configure testing

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  roots: ['src'],
+  moduleNameMapper: {
+    '^ugrade/(.*)$': '<rootDir>/src/$1',
+  },
 }
