aef9cb4d625e1269868b8ae96a9907f2e525d297
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-19 20:56:42 +0700

    implement and test getClarificationById in clarification service

---
+++ server/src/clarification/inmemory/inmemory.test.ts
@@ -0,0 +1,113 @@
+import { Permission } from 'ugrade/auth'
+import { mockAuthService, MockedAuthService } from 'ugrade/auth/mocked'
+import { ValidationError } from 'yup'
+import { Clarification } from '../clarification'
+import { NoSuchClarification } from '../NoSuchClarification'
+import { InMemoryClarificationService } from './inmemory'
+
+describe('test in memory clarification service', () => {
+  const getService = (
+    clarifications: Clarification[] = []
+  ): [InMemoryClarificationService, MockedAuthService] => {
+    const authService = mockAuthService()
+    const clarificationService = new InMemoryClarificationService(
+      authService,
+      clarifications
+    )
+    return [clarificationService, authService]
+  }
+
+  const token = '12345678901234567890123456789009'
+  const cid = token
+
+  test('getClarificationById validation', async () => {
+    const [service, _] = getService()
+    await expect(
+      service.getClarificationById('ivalidtoken', 'invalidcid')
+    ).rejects.toBeInstanceOf(ValidationError)
+
+    await service
+      .getClarificationById(token, token)
+      .catch(err => expect(err).not.toBeInstanceOf(ValidationError))
+  })
+
+  test('getClarificationById should throw no such clarification', async () => {
+    const [service, _] = getService()
+    await expect(
+      service.getClarificationById(token, cid)
+    ).rejects.toBeInstanceOf(NoSuchClarification)
+  })
+
+  test('getClarificationById should throw no such clarification when clarification is in different contest', async () => {
+    const [service, authService] = getService()
+    authService.getMe.mockResolvedValue({ contestId: 'cid1' })
+    authService.getUserById.mockResolvedValue({ contestId: 'cid2' })
+    await expect(
+      service.getClarificationById(token, cid)
+    ).rejects.toBeInstanceOf(NoSuchClarification)
+  })
+
+  test('getClarificationById should throw no such clarification', async () => {
+    const [service, authService] = getService([
+      {
+        id: cid,
+        title: 'title1',
+        subject: 'subject1',
+        issuerId: 'uidanon',
+        issuedTime: new Date(),
+        entryIds: [],
+      },
+    ])
+    authService.getMe.mockResolvedValue({
+      id: 'uid1',
+      contestId: 'cid1',
+      permissions: [],
+    })
+    authService.getUserById.mockResolvedValue({ contestId: 'cid1' })
+    await expect(
+      service.getClarificationById(token, cid)
+    ).rejects.toBeInstanceOf(NoSuchClarification)
+  })
+
+  test('getClarificationById should resolved when accessing own clarification', async () => {
+    const clarif = {
+      id: cid,
+      title: 'title1',
+      subject: 'subject1',
+      issuerId: 'uid1',
+      issuedTime: new Date(),
+      entryIds: ['id1'],
+    }
+    const [service, authService] = getService([clarif])
+    authService.getMe.mockResolvedValue({
+      id: 'uid1',
+      contestId: 'cid1',
+      permissions: [],
+    })
+    authService.getUserById.mockResolvedValue({ contestId: 'cid1' })
+    await expect(service.getClarificationById(token, cid)).resolves.toEqual(
+      clarif
+    )
+  })
+
+  test('getClarificationById should resolved when has clarifications:read permission', async () => {
+    const clarif = {
+      id: cid,
+      title: 'title1',
+      subject: 'subject1',
+      issuerId: 'uid1',
+      issuedTime: new Date(),
+      entryIds: ['id1'],
+    }
+    const [service, authService] = getService([clarif])
+    authService.getMe.mockResolvedValue({
+      id: 'uid1',
+      contestId: 'cid1',
+      permissions: [Permission.ClarificationsRead],
+    })
+    authService.getUserById.mockResolvedValue({ contestId: 'cid1' })
+    await expect(service.getClarificationById(token, cid)).resolves.toEqual(
+      clarif
+    )
+  })
+})
--- server/src/clarification/inmemory/inmemory.ts
+++ server/src/clarification/inmemory/inmemory.ts
@@ -1,8 +1,35 @@
+import lodash from 'lodash'
+import { AuthService, Permission } from 'ugrade/auth'
 import { Clarification, ClarificationEntry } from '../clarification'
+import { NoSuchClarification } from '../NoSuchClarification'
 import { ClarificationService } from '../service'
 import { clarificationServiceValidator } from '../validation'
 
 export class InMemoryClarificationService implements ClarificationService {
+  private authService: AuthService
+  private clarifications: Clarification[]
+  private idClarifications: { [id: string]: Clarification }
+  private entries: ClarificationEntry[]
+  private idEntries: { [id: string]: ClarificationEntry }
+
+  constructor(
+    authService: AuthService,
+    clarifications: Clarification[] = [],
+    entries: ClarificationEntry[] = []
+  ) {
+    this.authService = authService
+
+    this.clarifications = lodash.cloneDeep(clarifications)
+    this.idClarifications = {}
+    for (const clarif of this.clarifications) {
+      this.idClarifications[clarif.id] = clarif
+    }
+
+    this.entries = lodash.cloneDeep(entries)
+    this.idEntries = {}
+    for (const entry of this.entries) this.idEntries[entry.id] = entry
+  }
+
   async getClarificationById(
     token: string,
     clarificationId: string
@@ -11,7 +38,26 @@ export class InMemoryClarificationService implements ClarificationService {
       token,
       clarificationId
     )
-    throw new Error('not yet implemented')
+
+    // throw NoSuchClarification
+    if (!this.idClarifications[clarificationId]) throw new NoSuchClarification()
+
+    // throw NoSuchClarification if clarification in different contest
+    const clarification = this.idClarifications[clarificationId]
+    const me = await this.authService.getMe(token)
+    const issuer = await this.authService.getUserById(clarification.issuerId)
+    if (me.contestId !== issuer.contestId) throw new NoSuchClarification()
+
+    // if has no permission to read all clarification
+    // and not own target clarification
+    if (
+      !me.permissions.includes(Permission.ClarificationsRead) &&
+      me.id !== clarification.issuerId
+    ) {
+      throw new NoSuchClarification()
+    }
+
+    return lodash.cloneDeep(clarification)
   }
 
   async getClarificationEntries(
