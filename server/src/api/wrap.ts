3f85aa74760e6dac15c15836197d0fd5aa0f58fc
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-19 08:24:16 +0700

    add classname when wrap

--- server/src/api/wrap.ts
+++ server/src/api/wrap.ts
@@ -1,17 +1,25 @@
 import { ApolloError } from 'apollo-server-core'
 import { AuthError } from 'ugrade/auth'
 import { ValidationError } from 'yup'
+import { ContestError } from 'ugrade/contest'
+import { LanguageError } from 'ugrade/language'
+import { ProfileError } from 'ugrade/profile/ProfileError'
 
 export const wrap = async <T>(promise: Promise<T>): Promise<T> => {
   try {
     return await promise
   } catch (error) {
     if (error instanceof ValidationError) {
-      throw new ApolloError(error.message, 'INVALID_INPUT', error.errors)
+      throw new ApolloError(error.message, 'InvalidInput', error.errors)
     }
-    if (error instanceof AuthError) {
-      throw new ApolloError(error.message, 'AUTH_ERROR')
+    if (
+      error instanceof AuthError ||
+      error instanceof ContestError ||
+      error instanceof LanguageError ||
+      error instanceof ProfileError
+    ) {
+      throw new ApolloError(error.message, error.constructor.name)
     }
-    throw error
+    throw new ApolloError(error.message, 'ServerError')
   }
 }
