9b860b0de2e93816dadf2f06e89258daf7905e0e
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-23 11:05:14 +0700

    update user permission

--- web/src/services/auth/UserPermission.ts
+++ web/src/services/auth/UserPermission.ts
@@ -1,45 +1,41 @@
 export enum UserPermission {
-  /**
-   * can update common contest information like name, description, start time,
-   * finish time, etc.
-   */
-  InfoUpdate = 'info:update',
-
-  /**
-   * Can create announcement to the contest
-   */
-  AnnouncementCreate = 'announcement:create',
-  AnnouncementRead = 'announcement:read',
-
-  ProblemsCreate = 'problems:create',
-  ProblemsRead = 'problems:read',
-  ProblemsReadDisabled = 'problems:read-disabled',
-  ProblemsUpdate = 'problems:update',
-  ProblemsDelete = 'problems:delete',
-
-  UsersInvite = 'users:invite',
-  UsersPermissionsUpdate = 'users:permissions:update',
-  UsersDelete = 'users:delete',
-
-  ProfilesRead = 'profiles:read',
+  InfoUpdate = 'InfoUpdate',
+  AnnouncementCreate = 'AnnouncementCreate',
+  AnnouncementRead = 'AnnouncementRead',
+  ClarificationsRead = 'ClarificationsRead', // can read all clarification
+  ClarificationsCreate = 'ClarificationsCreate', // can create and read own clarification
+  ClarificationReply = 'ClarificationReply', // can reply all clarification
+  ProblemsCreate = 'ProblemsCreate',
+  ProblemsRead = 'ProblemsRead',
+  ProblemsReadDisabled = 'ProblemsReadDisabled',
+  ProblemsUpdate = 'ProblemsUpdate',
+  ProblemsDelete = 'ProblemsDelete',
+  UsersInvite = 'UsersInvite',
+  UsersPermissionsUpdate = 'UsersPermissionsUpdate',
+  UsersDelete = 'UsersDelete',
+  ProfilesRead = 'ProfilesRead',
+  SubmissionsRead = 'SubmissionsRead', // can read all submission
+  SubmissionsCreate = 'SUbmissionsCreate', // can submit
 }
 
 export const adminPermissions = [
   UserPermission.InfoUpdate,
   UserPermission.AnnouncementCreate,
   UserPermission.AnnouncementRead,
-
+  UserPermission.ClarificationsRead,
+  UserPermission.ClarificationsCreate,
+  UserPermission.ClarificationReply,
   UserPermission.ProblemsCreate,
   UserPermission.ProblemsRead,
   UserPermission.ProblemsReadDisabled,
   UserPermission.ProblemsUpdate,
   UserPermission.ProblemsDelete,
-
   UserPermission.UsersInvite,
   UserPermission.UsersPermissionsUpdate,
   UserPermission.UsersDelete,
-
   UserPermission.ProfilesRead,
+  UserPermission.SubmissionsRead,
+  UserPermission.SubmissionsCreate,
 ]
 
 export const contestantPermissions = [UserPermission.AnnouncementRead, UserPermission.ProblemsRead]
