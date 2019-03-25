a87fc9e42742708353e6b4eb0deb90ceb97f19f5
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-25 19:43:42 +0700

    fix vgrade typo to ugrade

--- server/ugrade/urls.py
+++ server/ugrade/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
 from graphene_django.views import GraphQLView
-from vgrade.schema import schema
+from ugrade.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
