b2f59209c0fa162b5e8809e50dc46bacfd5768fd
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-25 22:08:10 +0700

    fix model and add fixtures for dev

--- server/contests/admin.py
+++ server/contests/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from contests.models import User, Contest, Permission, Language
+from contests.models import User, Contest, Permission, Language, Problem
 
 admin.site.register(Contest)
 admin.site.register(User)
 admin.site.register(Permission)
 admin.site.register(Language)
+admin.site.register(Problem)
--- server/contests/models.py
+++ server/contests/models.py
@@ -19,19 +19,19 @@ class Contest(models.Model):
     name = models.CharField(max_length=255, validators=[
         MinLengthValidator(4), MaxLengthValidator(255)
     ])
-    shortId = models.CharField(max_length=255, unique=True, validators=[
+    short_id = models.CharField(max_length=255, unique=True, validators=[
         validate_slug, MinLengthValidator(4), MaxLengthValidator(255)
     ])
-    shortDescription = models.CharField(max_length=255, validators=[
+    short_description = models.CharField(max_length=255, validators=[
         MinLengthValidator(4), MaxLengthValidator(255)
     ])
     description = models.TextField(validators=[
         MaxLengthValidator(4 * 1024 * 1024)
     ])
-    startTime = models.DateTimeField()
+    start_time = models.DateTimeField()
     freezed = models.BooleanField()
-    finishTime = models.DateTimeField()
-    permittedLanguages = models.ManyToManyField(Language)
+    finish_time = models.DateTimeField()
+    permitted_languages = models.ManyToManyField(Language)
 
     def __str__(self):
         return self.name
@@ -48,10 +48,10 @@ class Permission(models.Model):
 class User(models.Model):
     name = models.CharField('name', max_length=255, validators=[
         MinLengthValidator(4), MaxLengthValidator(255)
-    ])
+    ], null=True, default=None, blank=True)
     username = models.CharField('username', max_length=64, validators=[
         validate_slug, MinLengthValidator(4), MaxLengthValidator(64)
-    ])
+    ], null=True, default=None, blank=True)
     email = models.EmailField('email', max_length=255, validators=[
         validate_email, MinLengthValidator(4), MaxLengthValidator(255)
     ])
@@ -59,23 +59,26 @@ class User(models.Model):
     contest = models.ForeignKey(
         Contest, related_name='members', on_delete=models.CASCADE)
 
-    password = models.CharField(max_length=255)
-    signup_otc = models.CharField(max_length=32)
-    reset_password_otc = models.CharField(max_length=32)
+    password = models.CharField(
+        max_length=255, null=True, default=None, blank=True)
+    signup_otc = models.CharField(
+        max_length=32, null=True, default=None, blank=True)
+    reset_password_otc = models.CharField(
+        max_length=32, null=True, default=None, blank=True)
 
     class Meta:
-        unique_together = [('username', 'contest',), ('email', 'contest',)]
+        unique_together = [('username', 'contest'), ('email', 'contest')]
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Problem(models.Model):
-    shortId = models.CharField(max_length=255, unique=True, validators=[
-        validate_slug, MinLengthValidator(4), MaxLengthValidator(255)
+    short_id = models.CharField(max_length=255, validators=[
+        validate_slug, MinLengthValidator(1), MaxLengthValidator(255)
     ])
     name = models.CharField(max_length=255, validators=[
-        MinLengthValidator(4), MaxLengthValidator(255)
+        MinLengthValidator(1), MaxLengthValidator(255)
     ])
     statement = models.TextField(validators=[
         MaxLengthValidator(4 * 1024 * 1024)
@@ -84,7 +87,13 @@ class Problem(models.Model):
         Contest, related_name='problems', on_delete=models.CASCADE)
     disabled = models.BooleanField()
     order = models.IntegerField()
-    timeLimit = models.IntegerField()
+    time_limit = models.IntegerField()
     tolerance = models.FloatField()
-    memoryLimit = models.IntegerField()
-    outputLimit = models.IntegerField()
+    memory_limit = models.IntegerField()
+    output_limit = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = [('short_id', 'contest')]
