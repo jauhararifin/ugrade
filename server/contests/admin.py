085754dc151c7238ba59cc4293feb7f5c9f70a29
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-25 22:18:06 +0700

    enhance admin page

--- server/contests/admin.py
+++ server/contests/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from contests.models import User, Contest, Permission, Language, Problem
 
-admin.site.register(Contest)
-admin.site.register(User)
-admin.site.register(Permission)
+
+@admin.register(Contest)
+class ContestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'short_id', 'short_description',
+                    'start_time', 'freezed', 'finish_time')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'contest', 'username', 'name', 'email')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description')
+
+
 admin.site.register(Language)
-admin.site.register(Problem)
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'contest', 'short_id', 'name', 'disabled',
+                    'time_limit', 'tolerance', 'memory_limit', 'output_limit')
+    list_display_links = ('id', 'name')
