77a99e01ef4cd3bafec6e46a24e7b54ece555ff7
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-26 21:00:54 +0700

    add top navigation bar

---
+++ ui/src/scenes/Dashboard/TopNavigationBar/TopNavigationBar.tsx
@@ -0,0 +1,79 @@
+import {
+  Alignment,
+  Breadcrumbs,
+  Button,
+  Classes,
+  IBreadcrumbProps,
+  Menu,
+  MenuItem,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  NavbarHeading,
+  Popover,
+  Position,
+} from '@blueprintjs/core'
+import classNames from 'classnames'
+import React, { FunctionComponent } from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth, useContest, useProblem, useRouting } from '../../../app'
+import { showSuccessToast, useContestOnly } from '../../../common'
+import { getBreadcrumb } from './breadcrumb'
+
+export type TopNavigationBarBreadcrumb = IBreadcrumbProps
+
+export const TopNavigationBar: FunctionComponent = () => {
+  useContestOnly()
+  const authStore = useAuth()
+  const user = authStore.me
+  const contestStore = useContest()
+  const problemStore = useProblem()
+  const routingStore = useRouting()
+  const breadcrumbs = getBreadcrumb(routingStore.location, contestStore.current, problemStore.problems)
+
+  const breadcrumbWithRouter = breadcrumbs.map(breadcrumbItem => ({
+    ...breadcrumbItem,
+    onClick: () => {
+      if (breadcrumbItem.href) routingStore.push(breadcrumbItem.href)
+    },
+    href: undefined,
+  }))
+
+  const handleSignOut = async () => {
+    authStore.signOut()
+    showSuccessToast('Signed Out')
+  }
+
+  return (
+    <Navbar>
+      <NavbarGroup align={Alignment.LEFT}>
+        <Link to='/contest'>
+          <NavbarHeading>UGrade</NavbarHeading>
+        </Link>
+        <NavbarDivider />
+        <Breadcrumbs items={breadcrumbWithRouter} />
+      </NavbarGroup>
+      <NavbarGroup align={Alignment.RIGHT}>
+        <Popover
+          disabled={!user}
+          content={
+            <Menu>
+              <MenuItem onClick={handleSignOut} text='Sign Out' />
+            </Menu>
+          }
+          position={Position.BOTTOM}
+        >
+          <Button
+            icon='caret-down'
+            large={true}
+            rightIcon='user'
+            className={classNames(Classes.MINIMAL, {
+              [Classes.SKELETON]: !user,
+            })}
+            text={(user && user.name) || 'Test'}
+          />
+        </Popover>
+      </NavbarGroup>
+    </Navbar>
+  )
+}
