a663c6e85ad662c4338de7ca04279be4b819a33d
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-27 15:49:12 +0700

    add common component in dashboard ui

---
+++ ui/src/scenes/Dashboard/components/SimpleLoading/SimpleLoading.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react'
+import { TwoRowLoading } from '../../../../components/TwoRowLoading'
+import { Content } from '../Content'
+
+export const SimpleLoading: FunctionComponent = () => (
+  <Content>
+    <TwoRowLoading />
+  </Content>
+)
