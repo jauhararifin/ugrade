cf209e8246610ecaa6eef2be09fe9b41786567a4
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-30 19:54:12 +0700

    pack spec to tarfile when submit solution

--- server/grading/grader.py
+++ server/grading/grader.py
@@ -1,10 +1,44 @@
 import os
 import tarfile
+import tempfile
 
+from django.core.files import File
 from django.db import transaction
 from django_rq import job
 
 from .models import GradingGroup
+
+
+def pack_spec(submission_model):
+    problem = submission_model.problem
+
+    # create tar file temporarily
+    fd, tar_path = tempfile.mkstemp()
+    os.close(fd)
+
+    # define files to be added, and their target name
+    files = [
+        (problem.tcgen_source, 'tcgen'),
+        (problem.solution_source, 'solution'),
+        (problem.checker_source, 'checker'),
+        (submission_model.solution_source, 'submission'),
+    ]
+
+    # add files to tar
+    spec = tarfile.open(tar_path, 'w')
+    for f, arcname in files:
+        ext = os.path.splitext(f.name)[1]
+        with f.open() as source:
+            fd, filename = tempfile.mkstemp()
+            os.close(fd)
+            with open(filename, 'wb') as dest:
+                for chunk in source.chunks():
+                    dest.write(chunk)
+            spec.add(filename, arcname + ext)
+            os.remove(filename)
+    spec.close()
+
+    return tar_path
 
 
 @job
@@ -13,38 +47,14 @@ def grade_submission(submission_model):
     problem = submission_model.problem
     contest = problem.contest
 
-    # Packing tar files
-    spec = tarfile.open('spec.tar', 'w')
-
-    file = problem.tcgen_source
-    tarinfo = tarfile.TarInfo('storages/' + file.name)
-    tarinfo.size = file.size
-    spec.addfile(tarinfo, file.open('rb'))
-    print(file, tarinfo, tarinfo.size)
-
-    file = problem.solution_source
-    tarinfo = tarfile.TarInfo('storages/' + file.name)
-    tarinfo.size = file.size
-    spec.addfile(tarinfo, file.open('rb'))
-    print(file, tarinfo, tarinfo.size)
-
-    file = problem.checker_source
-    tarinfo = tarfile.TarInfo('storages/' + file.name)
-    tarinfo.size = file.size
-    spec.addfile(tarinfo, file.open('rb'))
-    print(file, tarinfo, tarinfo.size)
-
-    file = submission_model.solution_source
-    tarinfo = tarfile.TarInfo('storages/' + file.name)
-    tarinfo.size = file.size
-    spec.addfile(tarinfo, file.open('rb'))
-    print(file, tarinfo, tarinfo.size)
-    spec.close()
-
+    # create grading group
     ggroup = GradingGroup(submission=submission_model,
                           verdict='PENDING',
                           grading_size=contest.grading_size)
-
     ggroup.save()
 
-    print("Grading submission {}".format(submission_model))
+    # add spec file
+    tar_path = pack_spec(submission_model)
+    with open(tar_path, 'r') as tar_file:
+        ggroup.spec.save('spec', File(tar_file))
+    os.remove(tar_path)
