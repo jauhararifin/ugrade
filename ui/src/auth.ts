3aa721cdf4633cc6cf0223498291727c64a5d45c
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-02 10:58:01 +0700

    add simple redirect hook

--- ui/src/auth.ts
+++ ui/src/auth.ts
@@ -1,9 +1,17 @@
 import { setContext } from 'apollo-link-context'
+import { useEffect } from 'react'
+import { useRouting } from './routing'
 
 export const TOKEN_STORAGE_KEY = 'ugrade.auth.token'
 
-export const authLink = setContext((_, { headers }) => {
+export function getToken(): string | undefined {
   const token = sessionStorage.getItem(TOKEN_STORAGE_KEY) || localStorage.getItem(TOKEN_STORAGE_KEY)
+  if (token) return token
+  return undefined
+}
+
+export const authLink = setContext((_, { headers }) => {
+  const token = getToken()
   return {
     headers: {
       ...headers,
@@ -11,3 +19,19 @@ export const authLink = setContext((_, { headers }) => {
     },
   }
 })
+
+export function usePublicOnly(to: string = '/contest') {
+  const routing = useRouting()
+  const isSignedIn = getToken()
+  useEffect(() => {
+    if (isSignedIn) routing.push(to)
+  }, [isSignedIn])
+}
+
+export function useContestOnly(to: string = '/enter-contest') {
+  const routing = useRouting()
+  const isSignedIn = getToken()
+  useEffect(() => {
+    if (!isSignedIn) routing.push(to)
+  }, [isSignedIn])
+}
--- ui/src/scenes/Home/Home.tsx
+++ ui/src/scenes/Home/Home.tsx
@@ -1,9 +1,10 @@
+import { usePublicOnly } from '@/auth'
 import { title } from '@/window'
 import React, { FunctionComponent } from 'react'
 import { HomeView } from './HomeView'
 
 export const Home: FunctionComponent = () => {
-  // usePublicOnly()
+  usePublicOnly()
   title('UGrade | Home')
   return <HomeView />
 }
