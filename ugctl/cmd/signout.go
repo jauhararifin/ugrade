1f8c9190d046215a3c9c8df6ed2acf7e51d01871
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-05 20:48:42 +0700

    fix signout command

--- ugctl/cmd/signout.go
+++ ugctl/cmd/signout.go
@@ -2,23 +2,29 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/jauhararifin/ugrade/ugctl/ugrade"
 	"github.com/spf13/cobra"
 )
 
-// signoutCmd represents the signout command
+func doSignOut(cmd *cobra.Command, args []string) error {
+	client := ugrade.NewClient()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := client.SignOut(ctx); err != nil {
+		return err
+	}
+	fmt.Println("\xE2\x9C\x94 signed out")
+	return nil
+}
+
 var signoutCmd = &cobra.Command{
 	Use:   "signout",
 	Short: "Sign Out from your current contest",
 	Long:  `This command basically just delete your session file`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		client := ugrade.NewClient()
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		return client.SignOut(ctx)
-	},
+	RunE:  doSignOut,
 }
 
 func init() {
