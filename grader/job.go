353c0c9d365076bb478fb641d2e9cc1ced33ab55
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-06 12:45:17 +0700

    add stringer in job and job result

--- grader/job.go
+++ grader/job.go
@@ -1,6 +1,9 @@
 package grader
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // Job represent grading job in ugrade.
 type Job struct {
@@ -8,9 +11,19 @@ type Job struct {
 	Spec  io.ReadCloser
 }
 
+func (job Job) String() string {
+	// TODO: use json
+	return job.Token
+}
+
 // JobResult respresent of result after job finished.
 type JobResult struct {
 	Job     Job
 	Verdict string
 	Output  io.ReadCloser
+}
+
+func (jobResult JobResult) String() string {
+	// TODO: use json
+	return fmt.Sprintf("%s - %s", jobResult.Job, jobResult.Verdict)
 }
