d1b08931334a83e0bdf43056a381acbbdc0a1bb9
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-08 22:36:57 +0700

    check contestant answer using checker

--- grader/worker/execute.go
+++ grader/worker/execute.go
@@ -3,7 +3,6 @@ package worker
 import (
 	"context"
 	"io/ioutil"
-	"math/rand"
 	"strings"
 
 	"github.com/jauhararifin/ugrade/grader"
@@ -43,7 +42,7 @@ func (worker *defaultWorker) Execute(ctx context.Context, job grader.Job) (*grad
 	}
 
 	// generate testcase suite
-	_, err = worker.generateTCOutputs(ctx, *tcin, compiledJury)
+	juryOutputs, err := worker.generateTCOutputs(ctx, *tcin, compiledJury)
 	if err != nil {
 		return nil, errors.Wrap(err, "error generating testcase outputs")
 	}
@@ -55,27 +54,25 @@ func (worker *defaultWorker) Execute(ctx context.Context, job grader.Job) (*grad
 	}
 
 	// run contestant solution
-	_, err = worker.generateContestantOutputs(ctx, *tcin, compiledContestant)
+	contestantOutputs, err := worker.generateContestantOutputs(ctx, *tcin, compiledContestant)
 	if err != nil {
 		return nil, errors.Wrap(err, "error executing contestant solution")
 	}
 
 	// compile checker
-	_, err = worker.compileChecker(ctx, *specs)
+	compiledChecker, err := worker.compileChecker(ctx, *specs)
 	if err != nil {
 		return nil, errors.Wrap(err, "error compiling checker")
 	}
 
-	if rand.Intn(2) == 0 {
-		return &grader.JobResult{
-			Job:     job,
-			Verdict: grader.AC,
-			Output:  ioutil.NopCloser(strings.NewReader("i just randomize the verdict and voila, you got AC")),
-		}, nil
+	grading, err := worker.generateGrading(ctx, compiledChecker, *contestantOutputs, *juryOutputs)
+	if err != nil {
+		return nil, errors.Wrap(err, "error run checker")
 	}
+
 	return &grader.JobResult{
 		Job:     job,
-		Verdict: grader.WA,
+		Verdict: grading.verdict,
 		Output:  ioutil.NopCloser(strings.NewReader("i just randomize the verdict and unfortunately you got WA")),
 	}, nil
 }
