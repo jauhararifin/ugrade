98832c1a53ff617ff65287ee52189c64286ddc84
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-10 23:20:20 +0700

    add huge io example program

--- examples/programs/huge_io.cpp
+++ examples/programs/huge_io.cpp
@@ -1,14 +1,21 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
   ios::sync_with_stdio(0);
-  while(1) {
-    cout<<(rand() % 26 + 'a');
-    if (rand()%100 == 0) {
-      cout<<endl;
-    }
+  int n = 4 * 1024;
+  char *buff = (char *)malloc(n);
+  while (1)
+  {
+    for (int i = 0; i < n; i++)
+      buff[i] = rand() % 26 + 'a';
+    buff[n - 1] = 0;
+    printf("%s\n", buff);
+    fflush(stdout);
   }
+  free(buff);
   return 0;
 }
