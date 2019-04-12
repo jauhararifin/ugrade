38466168699820b54f78ee0b2e3613feff2cb34f
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-12 10:20:02 +0700

    add debug and trace level logging flag

--- grader/ugrader/cmd/main.go
+++ grader/ugrader/cmd/main.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +11,20 @@ var rootCmd = &cobra.Command{
 	Long:          `This program fetch job periodically from server, execute it and send the result back to server.`,
 	SilenceUsage:  true,
 	SilenceErrors: false,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logrus.SetLevel(logrus.InfoLevel)
+		if ok, _ := cmd.PersistentFlags().GetBool("debug"); ok {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		if ok, _ := cmd.PersistentFlags().GetBool("trace"); ok {
+			logrus.SetLevel(logrus.TraceLevel)
+		}
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().Bool("debug", false, "show debug log")
+	rootCmd.PersistentFlags().Bool("trace", false, "show trace log")
 }
 
 // Execute run ugrader as cli.
